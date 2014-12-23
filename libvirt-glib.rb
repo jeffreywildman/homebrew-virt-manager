cb9f18aecff4c8b78dbf760e1c1aecfbd25d4f4a
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2014-12-22 20:50:16 -0600

    New formula for libvirt-glib

---
+++ libvirt-glib.rb
@@ -0,0 +1,109 @@
+class LibvirtGlib < Formula
+  homepage "http://libvirt.org/"
+  url "ftp://ftp.libvirt.org/libvirt/glib/libvirt-glib-0.1.9.tar.gz"
+  sha1 "ad54f51bf2afd5c7a23b7ee81a43dc7e61eadce2"
+
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gobject-introspection" => :build
+
+  depends_on "glib"
+  depends_on "libxml2"
+  depends_on "libvirt"
+
+  patch :DATA # remove unsupported linker option: --version-script
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-introspection",
+                          "--prefix=#{prefix}"
+
+    system "make", "install"
+  end
+end
+__END__
+diff --git a/libvirt-gconfig/Makefile.am b/libvirt-gconfig/Makefile.am
+index a9a6591..ca83fca 100644
+--- a/libvirt-gconfig/Makefile.am
++++ b/libvirt-gconfig/Makefile.am
+@@ -213,8 +213,8 @@ libvirt_gconfig_1_0_la_DEPENDENCIES = \
+ libvirt_gconfig_1_0_la_LDFLAGS = \
+ 			$(COVERAGE_CFLAGS:-f%=-Wc,f%) \
+ 			$(CYGWIN_EXTRA_LDFLAGS) $(MINGW_EXTRA_LDFLAGS) \
+-			-Wl,--version-script=$(srcdir)/libvirt-gconfig.sym \
+ 			-version-info $(LIBVIRT_GLIB_VERSION_INFO)
++#			-Wl,--version-script=$(srcdir)/libvirt-gconfig.sym \
+ 
+ BUILT_SOURCES = $(GCONFIG_GENERATED_FILES)
+ 
+diff --git a/libvirt-gconfig/Makefile.in b/libvirt-gconfig/Makefile.in
+index 42e4352..67d7fae 100644
+--- a/libvirt-gconfig/Makefile.in
++++ b/libvirt-gconfig/Makefile.in
+@@ -747,8 +747,8 @@ libvirt_gconfig_1_0_la_DEPENDENCIES = \
+ libvirt_gconfig_1_0_la_LDFLAGS = \
+ 			$(COVERAGE_CFLAGS:-f%=-Wc,f%) \
+ 			$(CYGWIN_EXTRA_LDFLAGS) $(MINGW_EXTRA_LDFLAGS) \
+-			-Wl,--version-script=$(srcdir)/libvirt-gconfig.sym \
+ 			-version-info $(LIBVIRT_GLIB_VERSION_INFO)
++#			-Wl,--version-script=$(srcdir)/libvirt-gconfig.sym \
+ 
+ BUILT_SOURCES = $(GCONFIG_GENERATED_FILES)
+ CLEANFILES = $(BUILT_SOURCES) $(am__append_2)
+diff --git a/libvirt-glib/Makefile.am b/libvirt-glib/Makefile.am
+index a48cfbb..d865a63 100644
+--- a/libvirt-glib/Makefile.am
++++ b/libvirt-glib/Makefile.am
+@@ -34,8 +34,8 @@ libvirt_glib_1_0_la_DEPENDENCIES = \
+ libvirt_glib_1_0_la_LDFLAGS = \
+ 			$(COVERAGE_CFLAGS:-f%=-Wc,f%) \
+ 			$(CYGWIN_EXTRA_LDFLAGS) $(MINGW_EXTRA_LDFLAGS) \
+-			-Wl,--version-script=$(srcdir)/libvirt-glib.sym \
+ 			-version-info $(LIBVIRT_GLIB_VERSION_INFO)
++#			-Wl,--version-script=$(srcdir)/libvirt-glib.sym \
+ 
+ # .libs/libvirt-glib.so is built by libtool as a side-effect of the Makefile
+ # rule for libosvirt-glib.la.  However, checking symbols relies on Linux ELF layout
+diff --git a/libvirt-glib/Makefile.in b/libvirt-glib/Makefile.in
+index 3523684..47c4417 100644
+--- a/libvirt-glib/Makefile.in
++++ b/libvirt-glib/Makefile.in
+@@ -436,8 +436,8 @@ libvirt_glib_1_0_la_DEPENDENCIES = \
+ libvirt_glib_1_0_la_LDFLAGS = \
+ 			$(COVERAGE_CFLAGS:-f%=-Wc,f%) \
+ 			$(CYGWIN_EXTRA_LDFLAGS) $(MINGW_EXTRA_LDFLAGS) \
+-			-Wl,--version-script=$(srcdir)/libvirt-glib.sym \
+ 			-version-info $(LIBVIRT_GLIB_VERSION_INFO)
++#			-Wl,--version-script=$(srcdir)/libvirt-glib.sym \
+ 
+ INTROSPECTION_GIRS = $(am__append_1)
+ CLEANFILES = $(am__append_2)
+diff --git a/libvirt-gobject/Makefile.am b/libvirt-gobject/Makefile.am
+index 7163c7d..8a379b0 100644
+--- a/libvirt-gobject/Makefile.am
++++ b/libvirt-gobject/Makefile.am
+@@ -90,8 +90,8 @@ libvirt_gobject_1_0_la_DEPENDENCIES = \
+ libvirt_gobject_1_0_la_LDFLAGS = \
+ 			$(COVERAGE_CFLAGS:-f%=-Wc,f%) \
+ 			$(CYGWIN_EXTRA_LDFLAGS) $(MINGW_EXTRA_LDFLAGS) \
+-			-Wl,--version-script=$(srcdir)/libvirt-gobject.sym \
+ 			-version-info $(LIBVIRT_GLIB_VERSION_INFO)
++#			-Wl,--version-script=$(srcdir)/libvirt-gobject.sym \
+ 
+ libvirt-gobject-enums.c: $(libvirt_gobject_1_0_la_HEADERS)
+ 	$(AM_V_GEN)glib-mkenums \
+diff --git a/libvirt-gobject/Makefile.in b/libvirt-gobject/Makefile.in
+index 26e0df6..0ffa15c 100644
+--- a/libvirt-gobject/Makefile.in
++++ b/libvirt-gobject/Makefile.in
+@@ -520,8 +520,8 @@ libvirt_gobject_1_0_la_DEPENDENCIES = \
+ libvirt_gobject_1_0_la_LDFLAGS = \
+ 			$(COVERAGE_CFLAGS:-f%=-Wc,f%) \
+ 			$(CYGWIN_EXTRA_LDFLAGS) $(MINGW_EXTRA_LDFLAGS) \
+-			-Wl,--version-script=$(srcdir)/libvirt-gobject.sym \
+ 			-version-info $(LIBVIRT_GLIB_VERSION_INFO)
++#			-Wl,--version-script=$(srcdir)/libvirt-gobject.sym \
+ 
+ BUILT_SOURCES = $(GOBJECT_GENERATED_FILES)
+ CLEANFILES = $(BUILT_SOURCES) $(am__append_2)
