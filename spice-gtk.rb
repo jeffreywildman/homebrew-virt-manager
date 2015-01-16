a2880e6be197f3b1b8cc6a86d50d26d95aa1059c
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2015-01-16 01:33:10 -0500

    New formula for spice-gtk

---
+++ spice-gtk.rb
@@ -0,0 +1,32 @@
+class SpiceGtk < Formula
+  homepage "http://www.spice-space.org/home.html"
+  url "http://www.spice-space.org/download/gtk/spice-gtk-0.26.tar.bz2"
+  sha1 "c84ffe620cc5b7c9f91ffaef4e4371636b472fa1"
+
+  depends_on "gobject-introspection" => :build
+  depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "vala" => :build
+
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gtk+3"
+  depends_on "jpeg"
+  depends_on "pango"
+  depends_on "pixman"
+  # TODO: audio
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-gtk=3.0",
+                          "--enable-introspection",
+                          "--enable-vala",
+                          "--with-audio=no",
+                          "--with-coroutine=gthread",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
