a5852e14424270e5f0d3327b1f55c208bc839c67
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2015-01-15 23:13:52 -0500

    Organize and add missing deps for gtk-vnc

--- gtk-vnc.rb
+++ gtk-vnc.rb
@@ -3,18 +3,23 @@ class GtkVnc < Formula
   url "https://download.gnome.org/sources/gtk-vnc/0.5/gtk-vnc-0.5.3.tar.xz"
   sha1 "37545223e944d7083d5ae5b945431d8d14eddd47"
 
-  depends_on "pkg-config" => :build
+  depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "vala" => :build
-  depends_on "gobject-introspection" => :build
 
-  depends_on "gtk+3"
-  depends_on "gnutls"
+  depends_on "atk"
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+  depends_on "gettext"
   depends_on "glib"
-  #depends_on :x11 # if your formula requires any X11/XQuartz components
-
-  # TODO: audio?
+  depends_on "gnutls"
+  depends_on "gtk+3"
+  depends_on "libgcrypt"
+  depends_on "libgpg-error"
+  depends_on "pango"
+  # TODO: sound
   #depends_on "pulseaudio"
 
   def install
