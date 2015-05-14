8d4019f1420d369b73180608103854e9492da83d
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2015-05-14 14:01:20 -0400

    spice-gtk 0.28
    * Add libtool build dependency.
    * Add usbredir to successfully compile.

--- spice-gtk.rb
+++ spice-gtk.rb
@@ -1,11 +1,12 @@
 class SpiceGtk < Formula
   homepage "http://www.spice-space.org/home.html"
-  url "http://www.spice-space.org/download/gtk/spice-gtk-0.26.tar.bz2"
-  sha256 "d61cabeb4ae03afb5bb921139491d1088ca0cdf77c7e70b8039fe62c2246e3f9"
+  url "http://www.spice-space.org/download/gtk/spice-gtk-0.28.tar.bz2"
+  sha256 "15aeeb63422cb3bfaa2edbf4602ed2025baa4455abfd1677f631245a0d0f81c4"
 
   depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
   depends_on "vala" => :build
 
   depends_on "cairo"
@@ -16,6 +17,7 @@ class SpiceGtk < Formula
   depends_on "jpeg"
   depends_on "pango"
   depends_on "pixman"
+  depends_on "usbredir"
   # TODO: audio
 
   def install
