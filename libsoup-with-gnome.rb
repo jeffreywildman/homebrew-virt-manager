26ae56d99958cfd8453caadb16b44420147f8cce
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2015-10-03 23:13:20 -0400

    libsoup-with-gnome 2.52.0

--- libsoup-with-gnome.rb
+++ libsoup-with-gnome.rb
@@ -1,21 +1,16 @@
 class LibsoupWithGnome < Formula
+  desc "HTTP client/server library for GNOME"
   homepage "https://live.gnome.org/LibSoup"
-  url "http://ftp.gnome.org/pub/GNOME/sources/libsoup/2.51/libsoup-2.51.3.tar.xz"
-  sha256 "ba7ef78e7e93cb08805daa1eb3a116914db7a5caf6a9f386817a7431ba5ef949"
-
-  bottle do
-    revision 1
-    sha1 "dc4dcb53c9da238e99feb055e70789eb54a2e98f" => :yosemite
-    sha1 "4c7ab2c3b250ace75f099f80dd9ba36f4b277550" => :mavericks
-    sha1 "693146c16d4105de3716252f2da9cf35156c0328" => :mountain_lion
-  end
+  url "https://download.gnome.org/sources/libsoup/2.52/libsoup-2.52.0.tar.xz"
+  sha256 "6c6c366622a1a9d938e0cea9b557fa536f088784251d31381ccd1b115a466785"
 
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "glib-networking"
   depends_on "gnutls"
   depends_on "sqlite"
-  depends_on "gobject-introspection" => :optional
+  depends_on "gobject-introspection"
+  depends_on "vala"
 
   def install
     args = [
@@ -25,12 +20,6 @@ class LibsoupWithGnome < Formula
       "--prefix=#{prefix}",
       "--disable-tls-check"
     ]
-
-    if build.with? "gobject-introspection"
-      args << "--enable-introspection"
-    else
-      args << "--disable-introspection"
-    end
 
     system "./configure", *args
     system "make", "install"
