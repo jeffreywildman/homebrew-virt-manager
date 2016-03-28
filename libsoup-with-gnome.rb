0195530e3ce36baa74ca1b560f19ae485d5c14e1
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2016-03-27 20:56:43 -0400

    libsoup-with-gnome 2.54.0.1

--- libsoup-with-gnome.rb
+++ libsoup-with-gnome.rb
@@ -1,8 +1,8 @@
 class LibsoupWithGnome < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://live.gnome.org/LibSoup"
-  url "https://download.gnome.org/sources/libsoup/2.53/libsoup-2.53.2.tar.xz"
-  sha256 "6d36c9924a517a4e455760d4c15995ef3b0d653693f5be99e56fd6fd23dfc413"
+  url "https://download.gnome.org/sources/libsoup/2.54/libsoup-2.54.0.1.tar.xz"
+  sha256 "ade4920166bd036e8890d04acdc135686d877670953949fa6245797c906e38e0"
 
   depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
