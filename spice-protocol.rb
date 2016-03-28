ef8ae05431f21fbf86851a77dbb234b320bf5608
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2016-03-27 20:55:33 -0400

    spice-protocol 0.12.11

--- spice-protocol.rb
+++ spice-protocol.rb
@@ -1,8 +1,8 @@
 class SpiceProtocol < Formula
   desc "Headers for SPICE protocol"
   homepage "http://www.spice-space.org/"
-  url "http://www.spice-space.org/download/releases/spice-protocol-0.12.10.tar.bz2"
-  sha256 "788f0d7195bec5b14371732b562eb55ca82712aab12273b0e87529fb30532efb"
+  url "http://www.spice-space.org/download/releases/spice-protocol-0.12.11.tar.bz2"
+  sha256 "22bae438bfb6c3245b195755c3f55e72c1f2bfae6f39c905b576fcb6c79b6330"
 
   def install
     system "./configure", "--disable-debug",
