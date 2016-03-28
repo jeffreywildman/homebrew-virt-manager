47d92806f8a0b9149582e4af15f099d1358ce631
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2016-03-27 20:56:13 -0400

    spice-gtk 0.31

--- spice-gtk.rb
+++ spice-gtk.rb
@@ -1,8 +1,8 @@
 class SpiceGtk < Formula
   desc "GTK client/libraries for SPICE"
   homepage "http://www.spice-space.org/home.html"
-  url "http://www.spice-space.org/download/gtk/spice-gtk-0.30.tar.bz2"
-  sha256 "e23323fa399efba17b3c18475ef10a29815851be5e7dbedd246b28ee8eb1379c"
+  url "http://www.spice-space.org/download/gtk/spice-gtk-0.31.tar.bz2"
+  sha256 "c72b4d202b1c0b71d6e24ce5caf914d6dddbcf4010d10db9c2d8e73af728c1ca"
 
   depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
