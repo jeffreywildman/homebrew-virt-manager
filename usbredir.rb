bea1ad498c3dfc8a7d3a0e496e9af327f9ab0267
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2018-12-25 15:04:51 -0600

    usbredir: 0.8.0

--- usbredir.rb
+++ usbredir.rb
@@ -1,8 +1,8 @@
 class Usbredir < Formula
   desc "USB traffic redirection library"
   homepage "https://www.spice-space.org"
-  url "https://www.spice-space.org/download/usbredir/usbredir-0.7.1.tar.bz2"
-  sha256 "407e9e27a1369f01264d5501ffbe88935ddd7d5de675f5835db05dc9c9ac56f3"
+  url "https://www.spice-space.org/download/usbredir/usbredir-0.8.0.tar.bz2"
+  sha256 "87bc9c5a81c982517a1bec70dc8d22e15ae197847643d58f20c0ced3c38c5e00"
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
@@ -10,6 +10,12 @@ class Usbredir < Formula
   depends_on "libusb"
 
   def install
+    #https://stackoverflow.com/questions/15860127/how-to-configure-tcp-keepalive-under-mac-os-x
+    inreplace "usbredirserver/usbredirserver.c" do |s|
+      s.gsub! "SOL_TCP", "IPPROTO_TCP"
+      s.gsub! "TCP_KEEPIDLE", "TCP_KEEPALIVE"
+    end
+
     system "./configure", "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
