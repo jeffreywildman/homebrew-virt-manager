4140e231a0bfa8d942e954a0665960514efaea94
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2019-01-06 15:48:28 -0500

    osinfo-db-tools: update runtime dependencies

--- osinfo-db-tools.rb
+++ osinfo-db-tools.rb
@@ -3,12 +3,14 @@ class OsinfoDbTools < Formula
   homepage "https://libosinfo.org"
   url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.2.0.tar.gz"
   sha256 "3b50829e5b58db15fe9fc8caf005aa18300262fea4562da0c2dfbe56355ff81f"
+  revision 1
 
   depends_on "intltool" => :build
-  depends_on "gettext" => :build
   depends_on "pkg-config" => :build
-  depends_on "libarchive" => :build
-  depends_on "glib" => :build
+
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "libarchive" # need >= 3.0.0
 
   def install
     args = %W[
