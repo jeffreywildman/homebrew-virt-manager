6bd9a87c1f72980a0549cfe9b2130d9bec474fb8
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2019-02-08 20:30:18 -0500

    osinfo-db-tools 1.3.0

--- osinfo-db-tools.rb
+++ osinfo-db-tools.rb
@@ -1,15 +1,15 @@
 class OsinfoDbTools < Formula
   desc "Tools for managing the libosinfo database files"
   homepage "https://libosinfo.org"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.2.0.tar.gz"
-  sha256 "3b50829e5b58db15fe9fc8caf005aa18300262fea4562da0c2dfbe56355ff81f"
-  revision 1
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.3.0.tar.gz"
+  sha256 "6e49bd94a9fb6fe371337e5a91a7e4ed6f370b2ca70385e0cab38d58ad7f07fa"
 
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
 
   depends_on "gettext"
   depends_on "glib"
+  depends_on "json-glib"
   depends_on "libarchive" # need >= 3.0.0
 
   def install
