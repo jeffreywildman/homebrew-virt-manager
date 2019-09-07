9069bac84c2abd530e903098421bdbec97484944
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2019-09-06 20:26:33 -0400

    virt-manager: update libvirt-python to 5.7.0

--- virt-manager.rb
+++ virt-manager.rb
@@ -5,7 +5,7 @@ class VirtManager < Formula
   homepage "https://virt-manager.org/"
   url "https://virt-manager.org/download/sources/virt-manager/virt-manager-2.2.1.tar.gz"
   sha256 "cfd88d66e834513e067b4d3501217e21352fadb673103bacb9e646da9f029a1b"
-  revision 1
+  revision 2
 
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
@@ -27,8 +27,8 @@ class VirtManager < Formula
   depends_on "vte3"
 
   resource "libvirt-python" do
-    url "https://libvirt.org/sources/python/libvirt-python-5.6.0.tar.gz"
-    sha256 "4df13fed9528a7eec153ab70196dcd481f6377ae5bf9ef84e07dfa1515202486"
+    url "https://libvirt.org/sources/python/libvirt-python-5.7.0.tar.gz"
+    sha256 "7af4ff057f36e1c806caca2b30abc020cdccd08d89e8898b54b66c1779d509a4"
   end
 
   resource "idna" do
