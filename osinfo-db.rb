059f4f22f6b1c141e244408f62f2ceec7d12b105
Jeffrey Wildman <jeffrey.wildman@gmail.com>
2019-09-28 14:13:56 -0400

    osinfo-db 20190920

--- osinfo-db.rb
+++ osinfo-db.rb
@@ -1,8 +1,8 @@
 class OsinfoDb < Formula
   desc "Libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-20190805.tar.xz"
-  sha256 "7da4477508cb2fb03da90123bafcc2ab1ff6c2d8f911a590cad9a202eb0448d1"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20190920.tar.xz"
+  sha256 "c8e32e93079a85b7d8c79ba9bc1a21c2e0403612f454049476242a2529a67c8a"
 
   depends_on "osinfo-db-tools" => :build
 
