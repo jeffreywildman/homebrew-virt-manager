4f97a2bc29de17343bf5bf4cd7f193510bbd25a2
Cyril Jouve <jv.cyril@gmail.com>
2021-05-14 00:34:05 +0200

    bump osinfo-db

--- osinfo-db.rb
+++ osinfo-db.rb
@@ -1,8 +1,8 @@
 class OsinfoDb < Formula
   desc "Libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-20210202.tar.xz"
-  sha256 "0bb56aeddf94a2cf48853c0e82cacaeb873d98c19590d81fbceadb06a391b11b"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20210426.tar.xz"
+  sha256 "66c93b4a5b319b2ff7d40360d9e94486a9a45f2c0dd1c30d4b31de4f223070d0"
 
   depends_on "osinfo-db-tools" => :build
 
