6b3fc9270d0a4c019add4d32b4c2944c591b4709
Cyril Jouve <jv.cyril@gmail.com>
2021-06-28 20:03:40 +0200

    bump minor

--- virt-manager.rb
+++ virt-manager.rb
@@ -5,7 +5,7 @@ class VirtManager < Formula
   homepage "https://virt-manager.org/"
   url "https://virt-manager.org/download/sources/virt-manager/virt-manager-3.2.0.tar.gz"
   sha256 "2b6fe3d90d89e1130227e4b05c51e6642d89c839d3ea063e0e29475fd9bf7b86"
-  revision 2
+  revision 3
 
   depends_on "docutils" => :build
   depends_on "gettext" => :build
